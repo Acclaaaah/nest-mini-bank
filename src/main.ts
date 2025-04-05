8c5bf2b07bcc9975c97b41c149c7a8ddabd1a6c3
fencim <fencim@gmail.com>
2025-04-05 11:39:49 +0800

    load configuration

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+  const appConfig = app.get(ConfigService);
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('MINI Bank')
     .setDescription('THIS IS MINI BANK SERVICE')
     .setVersion('1.0')
     .addTag('bank cash money')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory);
   
-  await app.listen(3000);
+  await app.listen(appConfig.get<number>('port') || 3000);
 }
 bootstrap();
