6846777182963a1becd055a9d357e0eea998ed2b
Amal Khan <salong.amalkhan@s.msumain.edu.ph>
2025-04-25 21:03:56 -0700

    Account Module

---
+++ src/accounts/account.module.ts
@@ -0,0 +1,13 @@
+// src/accounts/accounts.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './account.entity';
+import { AccountsService } from './accounts.service';
+import { AccountsController } from './accounts.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Account])],
+  controllers: [AccountsController],
+  providers: [AccountsService],
+})
+export class AccountsModule {}
