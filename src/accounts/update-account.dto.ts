af6a686852bb44e6515062367923f5ac4421df24
SystemError-404 <pandapatan.sp21@s.msumain.edu.ph>
2025-05-01 22:01:23 +0800

    moved to dto folder

--- src/accounts/update-account.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateAccountDto {
-  @ApiProperty({ required: false })
-  accountType?: 'checking' | 'savings';
-
-  @ApiProperty({ required: false })
-  balance?: number;
-}
