d9de9fe9c9744186874c41918d13efb363eda581
batokxxz <batocala.oo315@s.msumain.edu.ph>
2025-05-19 22:24:26 +0800

    All test are passed, I did my all my best sir. hehe

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -1,15 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { TransactionsService } from './transactions.service';
+import { Transaction } from './entities/transaction.entity';
+import { Account } from '../accounts/account.entity';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let transactionRepository: Repository<Transaction>;
+  let accountRepository: Repository<Account>;
+
+  const mockTransactionRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+    find: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  const mockAccountRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+    find: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useValue: mockTransactionRepository,
+        },
+        {
+          provide: getRepositoryToken(Account),
+          useValue: mockAccountRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
+    transactionRepository = module.get<Repository<Transaction>>(getRepositoryToken(Transaction));
+    accountRepository = module.get<Repository<Account>>(getRepositoryToken(Account));
   });
 
   it('should be defined', () => {
